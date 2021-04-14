6aab0a3ee2170fe4409dda723f5f795a70f0c084
박건우 <melonbottle@baggeon-uui-MacBookAir.local>
2021-04-14 18:12:45 +0900

    Revert "Modified Back-end Day5 Files"
    This reverts commit 174325845eae604a0fa1097d90fd13257474ff5f.

--- Back-end/source/day5/spring/src/main/java/emp/MemberVO.java
+++ Back-end/source/day5/spring/src/main/java/emp/MemberVO.java
@@ -1,7 +1,17 @@
 package emp;
 
 public class MemberVO implements VO {
-	int id;
-	String name;
+
+	@Override
+	public void setId(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
